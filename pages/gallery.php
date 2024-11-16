2e20ef3af7329e9fac386347e36752533c3871f0
YazenSH <87319592+YazenSH@users.noreply.github.com>
2024-11-17 01:16:52 +0300

    Update gallery.php

--- pages/gallery.php
+++ pages/gallery.php
@@ -7,17 +7,17 @@
             <img id="featured" src="../Images/canon-eos-r5.jpg" alt="Featured Image">
         </div>
         <div class="thumbnails">
-            <img class="thumb" src="../Images/canon-eos-r5.jpg" alt="Canon EOS R5">
-            <img class="thumb" src="../Images/Nikon70-200.jpg" alt="Nikon 70-200">
-            <img class="thumb" src="../Images/sony-alpha-a7-iii.jpg" alt="Sony Alpha">
-            <img class="thumb" src="../Images/tripod.jpg" alt="Tripod">
-            <img class="thumb" src="../Images/insta360.jpg" alt="Insta360">
-            <img class="thumb" src="../Images/micro128GB.jpg" alt="Memory Card">
+            <img class="thumb" src="../Images/canon-eos-r5.jpg" alt="Canon EOS R5"/>
+            <img class="thumb" src="../Images/Nikon70-200.jpg" alt="Nikon 70-200"/>
+            <img class="thumb" src="../Images/sony-alpha-a7-iii.jpg" alt="Sony Alpha"/>
+            <img class="thumb" src="../Images/tripod.jpg" alt="Tripod"/>
+            <img class="thumb" src="../Images/insta360.jpg" alt="Insta360"/>
+            <img class="thumb" src="../Images/micro128GB.jpg" alt="Memory Card"/>
         </div>
     </div>
 </div>
 
-<script>
+<script type="text/javascript">
 document.addEventListener('DOMContentLoaded', () => {
     const featured = document.getElementById('featured');
     const thumbnails = document.querySelectorAll('.thumb');
