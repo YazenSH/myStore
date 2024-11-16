7bac16bbcf624394e3d65cee87f8d9ea3f34cf15
YazenSH <87319592+YazenSH@users.noreply.github.com>
2024-11-17 02:07:10 +0300

    Update signup.php

--- pages/signup.php
+++ pages/signup.php
@@ -1,24 +1,24 @@
 <?php include '../includes/header.php'; ?>
 
 <div class="container">
-    <section class="signup-form box">
+    <div class="signup-form box">
         <h2>Sign Up</h2>
-        <form action="../php_actions/process_signup.php" method="POST" onsubmit="return validateSignupForm()">
+        <form action="../php_actions/process_signup.php" method="post" onsubmit="return validateSignupForm()">
             <div class="form-group">
                 <label for="name">Full Name:</label>
-                <input type="text" id="name" name="name" >
+                <input type="text" id="name" name="name"/>
             </div>
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" id="email" name="email" >
+                <input type="email" id="email" name="email"/>
             </div>
             <div class="form-group">
                 <label for="password">Password:</label>
-                <input type="password" id="password" name="password" >
+                <input type="password" id="password" name="password"/>
             </div>
             <div class="form-group">
                 <label for="confirm-password">Confirm Password:</label>
-                <input type="password" id="confirm-password" name="confirm-password" >
+                <input type="password" id="confirm-password" name="confirm-password"/>
             </div>
             <div class="form-group">
                 <input type="submit" value="Sign Up" class="btn">
@@ -27,8 +27,8 @@
                 <p>Already have an account? <a href="login.php">Login here</a></p>
             </div>
         </form>
-    </section>
+</div>
 </div>
 
-<script src="../js/validation.js"></script>
+<script src="../js/validation.js" type="text/css"></script>
 <?php include '../includes/footer.php'; ?>
