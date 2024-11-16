010a0f75c39224dcaf4fb2dd31342a35a53b07ff
YazenSH <87319592+YazenSH@users.noreply.github.com>
2024-11-17 02:04:19 +0300

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -2,20 +2,20 @@
 
 <!-- Main content container -->
 <div class="container">
-    <section class="contact-info box">
+    <div class="contact-info box">
         <h2>Get in Touch</h2>
         <p>
             We'd love to hear from you! Feel free to reach out with any questions or feedback.
         </p>
         <p>
-            <strong>Contact Information:</strong><br>
-            Yazan Yahya Alshuaibi<br>
-            ID: 2142647<br>
-            CPCS403<br>
-            Alsulamanyah, Jeddah City<br>
+            <strong>Contact Information:</strong><br/>
+            Yazan Yahya Alshuaibi<br/>
+            ID: 2142647<br/>
+            CPCS403<br/>
+            Alsulamanyah, Jeddah City<br/>
             <a href="mailto:yazanph@gmail.com">Email: yazanph@gmail.com</a>
         </p>
-    </section>
+</div>
 </div>
 
 <?php include '../includes/footer.php'; ?>
