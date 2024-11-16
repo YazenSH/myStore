108fea2bb619b9e831b67640d077822ebf988671
YazenSH <87319592+YazenSH@users.noreply.github.com>
2024-11-17 01:36:45 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,11 +2,17 @@
     <div class="footer">
         <p>&copy; 2024 Photography Store. All rights reserved.</p>
         <div>
-        <a href="http://validator.w3.org/check?uri=referer">
-                <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
+            <a href="https://validator.w3.org/check?uri=https://mystore-production-f6c9.up.railway.app/">
+                <img src="https://www.w3.org/Icons/valid-xhtml10" 
+                     alt="Valid XHTML 1.0 Strict" 
+                     height="31" 
+                     width="88" />
             </a>
-            <a href="https://jigsaw.w3.org/css-validator/check/referer">
-                <img src="https://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS"/>
+            <a href="https://jigsaw.w3.org/css-validator/validator?uri=https://mystore-production-f6c9.up.railway.app/">
+                <img src="https://jigsaw.w3.org/css-validator/images/vcss" 
+                     alt="Valid CSS" 
+                     height="31" 
+                     width="88"/>
             </a>
         </div>
     </div>
