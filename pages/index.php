c06487429d5085b186044674817afd7ab5e24ca7
YazenSH <87319592+YazenSH@users.noreply.github.com>
2024-11-16 17:12:04 +0300

    s

--- pages/index.php
+++ pages/index.php
@@ -1,10 +1,10 @@
 <?php 
-include '../includes/header.php';
-include '../db/connection.php';
+include dirname(__DIR__) . '/includes/header.php';
+//include '../db/connection.php';
 
 // Fetch only first 3 products
-$product_query = "SELECT * FROM products ORDER BY product_ID DESC LIMIT 3";
-$product_result = $conn->query($product_query);
+// $product_query = "SELECT * FROM products ORDER BY product_ID DESC LIMIT 3";
+// $product_result = $conn->query($product_query);
 ?>
 
 <!-- Introduction section -->
@@ -42,31 +42,6 @@ $product_result = $conn->query($product_query);
     </ul>
 </section>
 
-<!-- Featured Products -->
-<section class="featured">
-    <h2>Featured Products</h2>
-    <div class="product-row">
-        <?php while($product = $product_result->fetch_assoc()): ?>
-            <div class="product-item">
-                <img src="../<?php echo $product['image_path']; ?>" 
-                     alt="<?php echo htmlspecialchars($product['name']); ?>" 
-                     class="product-image">
-                <p><?php echo htmlspecialchars($product['name']); ?></p>
-                <p class="price"><?php echo number_format($product['price'], 2); ?> SR</p>
-                
-                <?php if(!isset($_SESSION['user_id'])): ?>
-                    <a href="login.php" class="add-to-cart-btn">Login to Buy</a>
-                <?php elseif(isset($_SESSION['is_admin']) && $_SESSION['is_admin']==1): ?>
-                    <button class="add-to-cart-btn" onclick="alert('Admins cannot add items to cart')">Add to Cart</button>
-                <?php else: ?>
-                    <form action="../php_actions/add_to_cart.php" method="POST">
-                        <input type="hidden" name="product_id" value="<?php echo $product['product_ID']; ?>">
-                        <button type="submit" class="add-to-cart-btn">Add to Cart</button>
-                    </form>
-                <?php endif; ?>
-            </div>
-        <?php endwhile; ?>
-    </div>
-</section>
 
-<?php include '../includes/footer.php'; ?>
+
+<?php include dirname(__DIR__) . '/includes/footer.php'; ?>
